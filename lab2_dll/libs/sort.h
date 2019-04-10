77471c48eae589ac59e7f25eee150ee3406f6d99
xofxela <46321394+xofxela@users.noreply.github.com>
2019-04-10 11:24:52 +0300

    rewrite sort algorithm

--- lab2_dll/libs/sort.h
+++ lab2_dll/libs/sort.h
@@ -1,11 +1,19 @@
 #ifndef SORT_H_
 #define SORT_H_
 
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <memory>
+
 extern "C" 
 {
-	__declspec(dllexport) void sort_file(std::vector<int>& numbers);
+	__declspec(dllexport) void mergeSort(std::vector<int> &array);
+	__declspec(dllexport) void merge(
+		std::vector<int> &array,
+		std::vector<int> &L,
+		std::vector<int> &R
+	);
 }
 #endif // SORT_H_
